67517f747fbf1a0ed3fcf293c5f1420f02a73fec
Birkin James Diana <birkin.diana@gmail.com>
2022-07-21 13:59:54 -0400

    writes json.

--- make_scanned_data_json.py
+++ make_scanned_data_json.py
@@ -13,8 +13,8 @@ log = logging.getLogger(__name__)
 
 data: dict = {}
 
-with open( CSV_PATH, encoding='utf-8' ) as file_handler:
-    csv_dict_reader = csv.DictReader( file_handler )
+with open( CSV_PATH, encoding='utf-8' ) as csv_file_handler:
+    csv_dict_reader = csv.DictReader( csv_file_handler )
         
     ## Convert each row into a dictionary and add it to data
     for rows in csv_dict_reader:
@@ -24,3 +24,7 @@ with open( CSV_PATH, encoding='utf-8' ) as file_handler:
         data[key] = rows
 
 log.debug( f'data, ``{pprint.pformat(data)}``' )
+
+jsn = json.dumps( data, sort_keys=True, indent=2 )
+with open( '../scanned_data.json', 'w', encoding='utf-8' ) as json_file_handler:
+    json_file_handler.write( jsn )
