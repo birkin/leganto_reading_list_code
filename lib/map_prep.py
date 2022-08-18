5f6cae3b27237fc199b2a48c560fc46fcb56f962
Birkin James Diana <birkin.diana@gmail.com>
2022-08-18 12:57:56 -0400

    converts boolean arguments to true bool vars.

---
+++ lib/map_prep.py
@@ -0,0 +1,6 @@
+from .loaders import load_csv
+
+def prepare_leganto_coursecode( ss_course_id: str, COURSES_FILEPATH: str ) -> str:
+    """ Looks up required fields from OIT course_info. """
+    return 'foo'
+    
