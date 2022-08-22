198bc799e8f744606334203775803328ddc82742
Birkin James Diana <birkin.diana@gmail.com>
2022-08-22 10:00:29 -0400

    Update loaders.py

--- lib/loaders.py
+++ lib/loaders.py
@@ -20,19 +20,39 @@ class OIT_Course_Loader( object ):
         """ Looks up required fields from OIT course_info. 
             Required leganto format: like `Summer 2022 DATA 2051 S01` (season, year, subject, code, section) """
         log.debug( f'preparing leganto coursecode for {ss_course_id}' )
-        leganto_course_code = ss_course_id
-        ( ss_subject, ss_code ) = ( ss_course_id[0:4], ss_course_id[4:] )
+        leganto_course_code = f'oit_course_code_not_found_for__{ss_course_id}'
+        ss_subject: str = ss_course_id[0:4]
+        ss_code: str = ss_course_id[4:]    
         log.debug( f'ss_subject, ``{ss_subject}``; ss_code, ``{ss_code}``' )
+        matcher: str = f'{ss_subject} {ss_code}'
         for entry in self.OIT_course_data:
             row: dict = entry
-            log.debug( f'row, ``{pprint.pformat(row)}``' )
+            log.debug( f'row, ``{row}``' )
             oit_course_code = row['COURSE_CODE']
-            log.debug( f'oit_course_code, ``{oit_course_code}``' )
-            parts: list = oit_course_code.split(' ')
-            [ oit_season, oit_year, oit_subject, oit_code, oit_section ] = parts
-            if ss_subject == oit_subject and ss_code == oit_code:
-                leganto_course_code = oit_course_code                
+            if matcher in oit_course_code:
+                leganto_course_code = oit_course_code
+                log.debug( 'match found; breaking' )
+                break
         log.debug( f'leganto_course_code, ``{leganto_course_code}``' )
         return leganto_course_code
+
+    # def prepare_leganto_coursecode( self, ss_course_id: str ) -> str:
+    #     """ Looks up required fields from OIT course_info. 
+    #         Required leganto format: like `Summer 2022 DATA 2051 S01` (season, year, subject, code, section) """
+    #     log.debug( f'preparing leganto coursecode for {ss_course_id}' )
+    #     leganto_course_code = ss_course_id
+    #     ( ss_subject, ss_code ) = ( ss_course_id[0:4], ss_course_id[4:] )
+    #     log.debug( f'ss_subject, ``{ss_subject}``; ss_code, ``{ss_code}``' )
+    #     for entry in self.OIT_course_data:
+    #         row: dict = entry
+    #         log.debug( f'row, ``{pprint.pformat(row)}``' )
+    #         oit_course_code = row['COURSE_CODE']
+    #         log.debug( f'oit_course_code, ``{oit_course_code}``' )
+    #         parts: list = oit_course_code.split(' ')
+    #         [ oit_season, oit_year, oit_subject, oit_code, oit_section ] = parts
+    #         if ss_subject == oit_subject and ss_code == oit_code:
+    #             leganto_course_code = oit_course_code                
+    #     log.debug( f'leganto_course_code, ``{leganto_course_code}``' )
+    #     return leganto_course_code
 
     ## end class OIT_Course_Loader()
