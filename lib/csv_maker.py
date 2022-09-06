7774fe4d6967b00e44f93bf2cbb3dfbd0c376915
Birkin James Diana <birkin.diana@gmail.com>
2022-09-06 11:10:42 -0400

    starts headers implementation.

--- lib/csv_maker.py
+++ lib/csv_maker.py
@@ -16,18 +16,22 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
+CSV_OUTPUT_DIR_PATH: str = os.environ['LGNT__CSV_OUTPUT_DIR_PATH']
 
-def create_csv( data: list ) -> None:
+
+def create_csv( data: list, headers: list ) -> None:
+    """ Credit: <https://python-adv-web-apps.readthedocs.io/en/latest/csv.html#writing-from-a-dictionary> """
+
     log.debug( f'data, ``{pprint.pformat(data)}``' )
 
     output_filename: str = f'reading_list_{datetime.datetime.now().isoformat()}.csv'.replace( ':', '-' )  # produces, eg, `reading_list_2022-09-06T10-59-04.345469`
     log.debug( f'output_filename, ``{output_filename}``' ) 
 
-    # # open a new file for writing - if file exists, contents will be erased
-    # csvfile = open('new_file.csv', 'w')
+    ## open a new file for writing - if file exists, contents will be erased
+    csvfile = open('new_file.csv', 'w')
 
-    # # set the headers
-    # headers = ['Presidency', 'President', 'Wikipedia_entry', 'Took_office', 'Left_office', 'Party', 'Home_state', 'Occupation', 'College', 'Age_when_took_office', 'Birth_date', 'Birthplace', 'Death_date', 'Location_death']
+    ## set the headers
+    headers = ['Presidency', 'President', 'Wikipedia_entry', 'Took_office', 'Left_office', 'Party', 'Home_state', 'Occupation', 'College', 'Age_when_took_office', 'Birth_date', 'Birthplace', 'Death_date', 'Location_death']
 
     # # make a new variable - c - for Python's DictWriter object -
     # # note that fieldnames is required
