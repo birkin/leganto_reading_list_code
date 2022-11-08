4ebd3c9abd943f4b94ce3de9720769396bc86acf
Birkin James Diana <birkin.diana@gmail.com>
2022-11-07 21:33:03 -0500

    stubs readings db-queries.

---
+++ lib/readings_extractor.py
@@ -0,0 +1,52 @@
+def get_book_readings( class_id: str ) -> list:
+    db_connection = get_db_connection()
+    sql = f"SELECT * FROM reserves.books, reserves.requests WHERE books.requestid = requests.requestid AND classid = {int(class_id)} ORDER BY `books`.`bk_title` ASC"
+    log.debug( f'sql, ``{sql}``' )
+    result_set: list = []
+    with db_connection:
+        with db_connection.cursor() as db_cursor:
+            db_cursor.execute( sql )
+            result_set = list( db_cursor.fetchall() )
+            assert type(result_set) == list
+    log.debug( '\n\n----------\nbooks\n----------' )
+    for entry in result_set:
+        log.debug( f'\n\nbook, ``{entry}``')
+    log.debug( '\n\n----------' )
+    return result_set
+
+
+def get_article_readings( class_id: str ) -> list:
+    db_connection = get_db_connection()
+    sql = f"SELECT * FROM reserves.articles, reserves.requests WHERE articles.requestid = requests.requestid AND classid = {int(class_id)} AND format = 'article' AND articles.requestid = requests.requestid AND articles.status != 'volume on reserve' AND articles.status != 'purchase requested' ORDER BY `articles`.`atitle` ASC"
+    log.debug( f'sql, ``{sql}``' )
+    result_set: list = []
+    with db_connection:
+        with db_connection.cursor() as db_cursor:
+            db_cursor.execute( sql )
+            result_set = list( db_cursor.fetchall() )
+            assert type(result_set) == list
+    log.debug( '\n\n----------\narticles\n----------' )
+    for entry in result_set:
+        log.debug( f'\n\narticle, ``{entry}``')
+        if entry['doi'][0:1] == ' ':
+            log.debug( 'cleaning doi' )
+            entry['doi'] = entry['doi'].strip()
+    log.debug( '\n\n----------' )
+    return result_set
+
+
+def get_excerpt_readings( class_id: str ) -> list:
+    db_connection = get_db_connection()
+    sql = f"SELECT * FROM reserves.articles, reserves.requests WHERE requests.classid = {int(class_id)} AND format = 'excerpt' AND articles.requestid = requests.requestid AND articles.status != 'volume on reserve' AND articles.status != 'purchase requested' ORDER BY `articles`.`atitle` ASC;"
+    log.debug( f'sql, ``{sql}``' )
+    result_set: list = []
+    with db_connection:
+        with db_connection.cursor() as db_cursor:
+            db_cursor.execute( sql )
+            result_set = list( db_cursor.fetchall() )
+            assert type(result_set) == list
+    log.debug( '\n\n----------\nexcerpts\n----------' )
+    for entry in result_set:
+        log.debug( f'\n\nexcerpt, ``{entry}``')
+    log.debug( '\n\n----------' )
+    return result_set
