8f58bf91e1178f513d1bd49688469dcc65e549b8
Birkin James Diana <birkin.diana@gmail.com>
2022-11-07 22:56:39 -0500

    builds out query code.

--- lib/readings_extractor.py
+++ lib/readings_extractor.py
@@ -1,5 +1,19 @@
+import logging, os
+
+from lib import db_stuff
+
+
+LOG_PATH: str = os.environ['LGNT__LOG_PATH']
+logging.basicConfig(
+    filename=LOG_PATH,
+    level=logging.DEBUG,
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S' )
+log = logging.getLogger(__name__)
+
+
 def get_book_readings( class_id: str ) -> list:
-    db_connection = get_db_connection()
+    db_connection = db_stuff.get_db_connection()
     sql = f"SELECT * FROM reserves.books, reserves.requests WHERE books.requestid = requests.requestid AND classid = {int(class_id)} ORDER BY `books`.`bk_title` ASC"
     log.debug( f'sql, ``{sql}``' )
     result_set: list = []
@@ -16,7 +30,7 @@ def get_book_readings( class_id: str ) -> list:
 
 
 def get_article_readings( class_id: str ) -> list:
-    db_connection = get_db_connection()
+    db_connection = db_stuff.get_db_connection()
     sql = f"SELECT * FROM reserves.articles, reserves.requests WHERE articles.requestid = requests.requestid AND classid = {int(class_id)} AND format = 'article' AND articles.requestid = requests.requestid AND articles.status != 'volume on reserve' AND articles.status != 'purchase requested' ORDER BY `articles`.`atitle` ASC"
     log.debug( f'sql, ``{sql}``' )
     result_set: list = []
@@ -36,7 +50,7 @@ def get_article_readings( class_id: str ) -> list:
 
 
 def get_excerpt_readings( class_id: str ) -> list:
-    db_connection = get_db_connection()
+    db_connection = db_stuff.get_db_connection()
     sql = f"SELECT * FROM reserves.articles, reserves.requests WHERE requests.classid = {int(class_id)} AND format = 'excerpt' AND articles.requestid = requests.requestid AND articles.status != 'volume on reserve' AND articles.status != 'purchase requested' ORDER BY `articles`.`atitle` ASC;"
     log.debug( f'sql, ``{sql}``' )
     result_set: list = []
@@ -50,3 +64,4 @@ def get_excerpt_readings( class_id: str ) -> list:
         log.debug( f'\n\nexcerpt, ``{entry}``')
     log.debug( '\n\n----------' )
     return result_set
+    
