ec96a1bc83699b6ad9731d8a80ae911af435ea5c
Birkin James Diana <birkin.diana@gmail.com>
2022-11-14 16:42:47 -0500

    adds course department, title, and course-code to audiolinks data.

--- lib/make_audiolinks_json_data.py
+++ lib/make_audiolinks_json_data.py
@@ -45,7 +45,7 @@ with db_connection.cursor() as db_cursor:
         classid = row['requests__classid']
         log.debug( f'classid, ``{classid}``' )
         if classid == None:
-            row['courseid'] = None
+            row['classes__courseid'] = None
         else:
             q2 = AUDIO_LINKS_SQL2.replace( '{classid}', str(classid) )
             log.debug( f'q2, ``{q2}``' )
@@ -55,10 +55,36 @@ with db_connection.cursor() as db_cursor:
             assert type(r_set2) == list
             log.debug( f'len(r_set2), ``{len(r_set2)}``' )
             r_set2_entry: dict = r_set2[0]
-            # row['courseid'] = r_set2_entry['courseid']
-            row['courseid'] = f'multiple tables may have course info for `courseid`, ``{r_set2_entry["courseid"]}``' 
+            row['classes__courseid'] = r_set2_entry['courseid']
 log.debug( f'result_set[0:20], after course_id lookup, ``{pprint.pformat(result_set[0:20])}``' )
 
+## add course_info --------------------------------------------------
+AUDIO_LINKS_SQL3 = os.environ['LGNT__AUDIO_LINKS_SQL3']
+db_connection: pymysql.connections.Connection = db_stuff.get_db_connection()
+with db_connection.cursor() as db_cursor:
+    for entry in result_set:
+        row: dict = entry
+        courseid = row['classes__courseid']
+        log.debug( f'courseid, ``{courseid}``' )
+        classid = row['requests__classid']
+        log.debug( f'classid, ``{classid}``' )
+        if courseid == None or classid == None:
+            row['banner_courses__course_title'] = None
+            row['banner_courses__subject'] = None
+            row['banner_courses__course'] = None
+        else:
+            q3 = AUDIO_LINKS_SQL3.replace( '{courseid}', str(courseid) ).replace( '{classid}', str(classid) )
+            log.debug( f'q3, ``{q3}``' )
+            r_set3: list = []
+            db_cursor.execute( q3 )
+            r_set3 = list( db_cursor.fetchall() )  # list() only needed for pylance type-checking
+            assert type(r_set3) == list
+            log.debug( f'len(r_set3), ``{len(r_set3)}``' )
+            r_set3_entry: dict = r_set3[0]
+            row['banner_courses__course_title'] = r_set3_entry['course_title']
+            row['banner_courses__subject'] = r_set3_entry['subject']
+            row['banner_courses__course'] = r_set3_entry['course']
+log.debug( f'result_set[0:20], after course lookup, ``{pprint.pformat(result_set[0:20])}``' )
 
 
 1/0
