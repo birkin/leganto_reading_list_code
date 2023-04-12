db496d75b77ee01359398e76603f93db22df0a22
Birkin James Diana <birkin.diana@gmail.com>
2023-04-12 10:36:16 -0400

    updates output-data-summary for step-1 (10...)

--- instructor_check_flow/10_prepare_oit_initial_subset.py
+++ instructor_check_flow/10_prepare_oit_initial_subset.py
@@ -29,12 +29,17 @@ OIT_COURSE_LIST_PATH: str = os.environ['LGNT__COURSES_FILEPATH']
 TARGET_SEASON: str = os.environ['LGNT__SEASON']
 TARGET_YEAR: str = os.environ['LGNT__YEAR']
 LEGIT_SECTIONS: str = json.loads( os.environ['LGNT__LEGIT_SECTIONS_JSON'] )
-OIT_SUBSET_01_OUTPUT_PATH: str = '%s/oit_subset_01.tsv' % os.environ['LGNT__CSV_OUTPUT_DIR_PATH']
+JSON_SUMMARY_DIR_PATH: str = os.environ['LGNT__JSON_DATA_DIR_PATH']
 log.debug( f'OIT_COURSE_LIST_PATH, ``{OIT_COURSE_LIST_PATH}``' )
 log.debug( f'TARGET_SEASON, ``{TARGET_SEASON}``' )
 log.debug( f'TARGET_YEAR, ``{TARGET_YEAR}``' )
 log.debug( f'LEGIT_SECTIONS, ``{LEGIT_SECTIONS}``' )
+
+## set other constants ----------------------------------------------
+OIT_SUBSET_01_OUTPUT_PATH: str = '%s/oit_subset_01.tsv' % os.environ['LGNT__CSV_OUTPUT_DIR_PATH']
+JSON_SUMMARY_PATH = '%s/oit_data_01a_summary.json' % JSON_SUMMARY_DIR_PATH
 log.debug( f'OIT_SUBSET_01_OUTPUT_PATH, ``{OIT_SUBSET_01_OUTPUT_PATH}``' )
+log.debug( f'JSON_SUMMARY_PATH, ``{JSON_SUMMARY_PATH}``' )
 
 
 ## controller -------------------------------------------------------
@@ -105,9 +110,12 @@ def main():
     log.debug( f'updated_buckets_dict, ``{pprint.pformat(buckets_dict)}``' )
 
     ## prep easyview output -----------------------------------------
-    # easyview_output = make_easyview_output( buckets_dict )
     easyview_output = make_easyview_output( buckets_dict, skipped_due_to_no_instructor )
     log.debug( f'easyview_output, ``{pprint.pformat(easyview_output)}``' )
+
+    ## write json summary -------------------------------------------
+    with open( JSON_SUMMARY_PATH, 'w' ) as f:
+        json.dump( easyview_output, f, indent=2 )
 
     ## write summer-2023 subset to file -----------------------------
     with open( OIT_SUBSET_01_OUTPUT_PATH, 'w' ) as f:
@@ -141,22 +149,38 @@ def parse_line( data_line: str, heading_line: str, line_number: int ) -> dict:
 
 def make_easyview_output( buckets_dict: dict, skipped_due_to_no_instructor: list ) -> dict:
     """ Prepares easyview output.
+        Takes category-key and all-list-values and makes a 'unique_values' key with the data being
+            ... a list of tuples, each tuple being ( value, count ).
         Called by main() """
     assert type(buckets_dict) == dict
     output_dict = {}
     for key in buckets_dict.keys():
         unsorted_unique_values = buckets_dict[key]['unique_values']
-        # sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: x[1], reverse=True )
-        ## sort by count and then by string
-        # sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: (x[1], x[0]), reverse=True )
-        ## sort by count-descending, and then by string-ascending
         sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: (-x[1], x[0]) )        
         output_dict[key] = sorted_unique_values
     output_dict['skipped_instructor_count_for_target_year_and_season_and_section'] = len( skipped_due_to_no_instructor )
-    # jsn = json.dumps( output_dict, indent=2 )
-    # with open( './output.json', 'w' ) as f:
-    #     f.write( jsn )
+    log.debug( f'output_dict, ``{pprint.pformat(output_dict)}``' )
     return output_dict
+
+
+# def make_easyview_output( buckets_dict: dict, skipped_due_to_no_instructor: list ) -> dict:
+#     """ Prepares easyview output.
+#         Called by main() """
+#     assert type(buckets_dict) == dict
+#     output_dict = {}
+#     for key in buckets_dict.keys():
+#         unsorted_unique_values = buckets_dict[key]['unique_values']
+#         # sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: x[1], reverse=True )
+#         ## sort by count and then by string
+#         # sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: (x[1], x[0]), reverse=True )
+#         ## sort by count-descending, and then by string-ascending
+#         sorted_unique_values = sorted( unsorted_unique_values, key=lambda x: (-x[1], x[0]) )        
+#         output_dict[key] = sorted_unique_values
+#     output_dict['skipped_instructor_count_for_target_year_and_season_and_section'] = len( skipped_due_to_no_instructor )
+#     # jsn = json.dumps( output_dict, indent=2 )
+#     # with open( './output.json', 'w' ) as f:
+#     #     f.write( jsn )
+#     return output_dict
 
 
 def add_counts( buckets_dict: dict ) -> dict:
@@ -254,129 +278,3 @@ def parse_course_code( data_line, i ):
 if __name__ == '__main__':
     main()
     sys.exit(0)
-
-
-   ## set up buckets
-    # missing_institutions = []
-    # missing_departments = []
-    # missing_numbers = []
-    # missing_years = []
-    # missing_terms = []
-    # missing_sections = []
-    # course_code_institutions = []
-    # course_code_departments = []
-    # # course_code_numbers = []  ## don't need to populate this
-    # course_code_years = []
-    # course_code_terms = []
-    # course_code_sections = []
-    # course_code_section_missing_count = 0
-    # summer_2023_lines = []
-
-    ## make summer-2023 subset -
-    # for i, data_line in enumerate( data_lines ):
-    #     log.debug( f'processing data_line, ``{data_line}``' )
-    #     parts = data_line.split( '\t' )
-    #     parts = [ part.strip() for part in parts ]
-    #     ## parse course code --------------------------------------------
-    #     course_code_parts: list = parts[0].split( '.' )
-    #     log.debug( f'processing course_code_parts, ``{course_code_parts}``' )
-    #     course_code_dict = {
-    #         'course_code_institution': course_code_parts[0],
-    #         'course_code_department': course_code_parts[1],
-    #         'course_code_number': course_code_parts[2],
-    #         'course_code_year_and_term': course_code_parts[3],
-    #         # 'course_code_section': course_code_parts[4] 
-    #         }
-    #     ## handle missing section
-    #     if len(course_code_parts) == 5:
-    #         course_code_dict['course_code_section'] = course_code_parts[4]
-    #     else:
-    #         log.debug( 'adding EMPTY section' )
-    #         course_code_dict['course_code_section'] = 'EMPTY'
-    #         course_code_section_missing_count += 1
-    #     ## handle year and term
-    #     course_code_year = course_code_parts[3].split('-')[0]
-    #     course_code_term = course_code_parts[3].split('-')[1]
-    #     course_code_dict['course_code_year'] = course_code_year
-    #     course_code_dict['course_code_term'] = course_code_term
-    #     if i < 5:
-    #         log.debug( f'course_code_dict, ``{pprint.pformat(course_code_dict)}``' )
-    #     # if i > 6:
-    #     #     break
-    #     ## evaluate for summer-2023 --------------------------------------
-
-#     ## checks...
-#     if course_code_dict['course_code_institution'].strip() == '':
-#         missing_institutions.append( course_code_dict )
-#     if course_code_dict['course_code_department'].strip() == '':
-#         missing_departments.append( course_code_dict )
-#     if course_code_dict['course_code_number'].strip() == '':
-#         missing_numbers.append( course_code_dict )
-#     if course_code_dict['course_code_year'].strip() == '':
-#         missing_years.append( course_code_dict )
-#     if course_code_dict['course_code_term'].strip() == '':
-#         missing_terms.append( course_code_dict )
-#     if course_code_dict['course_code_section'].strip() == '':
-#         missing_sections.append( course_code_dict )
-#     ## collect...
-#     if course_code_dict['course_code_institution'] not in course_code_institutions:
-#         course_code_institutions.append( course_code_dict['course_code_institution'] )
-#     if course_code_dict['course_code_department'] not in course_code_departments:       
-#         course_code_departments.append( course_code_dict['course_code_department'] )
-#     if course_code_dict['course_code_year'] not in course_code_years:
-#         course_code_years.append( course_code_dict['course_code_year'] )
-#     if course_code_dict['course_code_term'] not in course_code_terms:
-#         course_code_terms.append( course_code_dict['course_code_term'] )
-#     if course_code_dict['course_code_section'] not in course_code_sections:
-#         course_code_sections.append( course_code_dict['course_code_section'] )
-
-# ## log results ------------------------------------------------------
-# log.debug( f'OIT course entries count, ``{len(data_lines)}``' )
-# log.debug( f'missing_departments, ``{pprint.pformat(sorted(missing_departments))}``' )
-# log.debug( f'missing_numbers, ``{pprint.pformat(sorted(missing_numbers))}``' )
-# log.debug( f'missing_years, ``{pprint.pformat(sorted(missing_years))}``' )
-# log.debug( f'missing_terms, ``{pprint.pformat(sorted(missing_terms))}``' )
-# log.debug( f'missing_sections, ``{pprint.pformat(sorted(missing_sections))}``' )
-# log.debug( f'course_code_institutions, ``{pprint.pformat(sorted(course_code_institutions))}``' )
-# log.debug( f'course_code_departments, ``{pprint.pformat(sorted(course_code_departments))}``' )
-# # log.debug( f'course_code_numbers, ``{pprint.pformat(course_code_numbers)}``' )
-# log.debug( f'course_code_years, ``{pprint.pformat(sorted(course_code_years))}``' )
-# log.debug( f'course_code_terms, ``{pprint.pformat(sorted(course_code_terms))}``' )
-# log.debug( f'course_code_sections, ``{pprint.pformat(sorted(course_code_sections))}``' )
-# log.debug( f'course_code_section_missing_count, ``{course_code_section_missing_count}``' )
-
-
-# def make_easyview_output( buckets_dict: dict ) -> str:
-#     """ Prepares easyview output.
-#         Called by main() """
-#     assert type(buckets_dict) == dict
-#     output = ''
-#     for key in buckets_dict.keys():
-#         # output += f'{key}:\n'
-#         header = f'{key}:'
-#         data = ''
-#         for item in buckets_dict[key]['unique_values']:
-#             data += f'  {item[0]} ({item[1]})\n'
-#         ## sort by count
-#         data = ''.join( sorted( data.split('\n') ) )
-#         output += f'{header}\n{data}'
-
-#             # output += f'  {item[0]} ({item[1]})\n'
-#     return output
-
-
-# def make_easyview_output( buckets_dict: dict ) -> str:
-#     """ Prepares easyview output.
-#         Called by main() """
-#     assert type(buckets_dict) == dict
-#     output = ''
-#     for key in buckets_dict.keys():
-#         output += f'{key}:\n'
-#         for item in buckets_dict[key]['unique_values']:
-#             output += f'  {item[0]} ({item[1]})\n'
-#     return output
-
-
-## example course code
-# brown.afri.0090.2023-fall.s01
-
